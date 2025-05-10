e9acc3f66ac87d0058d791ab6a0ad2dfb1eba4bb
Julie Lausecker <julielausecker@gmail.com>
2025-05-10 12:20:10 +0200

    First commit

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$config = require__DIR__ . '/env.php';
+
+$host = $config['DB_HOST'];
+$bddname = $config['DB_NAME'];
+$user = $config['DB_USER'];
+$password = $config['DB_PASSWORD'];
+
+// Gestion des erreurs de connexion
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$bddname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur lors de la connexion à la BDD ! \n Erreur : " .  $e->getMessage();
+}
